6c1c37eddfc9c4e24882b3e3b50b9b5a65560dc5
km <kmf@kmf-Inspiron-N5110.(none)>
2012-11-25 17:11:38 +0400

    get

--- server.c
+++ server.c
@@ -5,22 +5,31 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-int main()
+int main(int argc, char** argv)
 {
+    if(argc <2)
+	{
+	  printf("./server max_numb_of_client");
+	  return;
+	}
     int sock, listener;
+    int numb_max_client = atoi(argv[1]);
+    int numb_client=0;
     struct sockaddr_in addr;
     char buf[1024];
     int bytes_read;
+    char* filename;
+    FILE *file;
 
     listener = socket(AF_INET, SOCK_STREAM, 0);
-    if(listener < 0)
+    if(listener < 0) 
     {
         perror("socket");
         exit(1);
     }
     
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(3425);
+    addr.sin_port = htons(3436);
     addr.sin_addr.s_addr = INADDR_ANY;
     if(bind(listener, (struct sockaddr *)&addr, sizeof(addr)) < 0)
     {
@@ -31,13 +40,16 @@ int main()
     listen(listener, 1);
     
     while(1)
-    {
-        sock = accept(listener, NULL, NULL);
-        if(sock < 0)
-        {
+    {  
+	if (numb_client< numb_max_client)
+	{	
+         sock = accept(listener, NULL, NULL);
+         numb_client++;
+          if(sock < 0)
+           {
             perror("accept");
             exit(3);
-        }
+           }
         
         switch(fork())
         {
@@ -48,10 +60,51 @@ int main()
         case 0:
             close(listener);
             while(1)
+            { 
+               
+            char c;
+            int i = 0;
+            memset(buf,0,1024);
+            filename = (char*)malloc(12 * sizeof(char));       
+            read(sock, buf, 1024);
+            printf("%s\n",buf);
+            strcpy(filename,buf);
+            if((file = fopen(filename, "r")) == NULL)
             {
-                bytes_read = recv(sock, buf, 1024, 0);
-                if(bytes_read <= 0) break;
-                send(sock, buf, bytes_read, 0);
+                memcpy(buf, "file not found\n",sizeof(buf));
+                send(sock, buf, sizeof(buf),0);
+                close(sock);
+                free(filename);
+            }
+            else
+            {
+                while((c = fgetc(file)) != EOF)
+                {
+                    buf[i] = c;
+                    i++;
+
+                    if((i == 1023))
+                    {
+
+                        printf("send\n");
+                        send(sock, buf, sizeof(buf), 0);
+                        i=0;
+                        memset(buf,0,sizeof(buf));
+
+                    }
+
+
+                }
+                if(i!=0)
+                {
+                    send(sock, buf, sizeof(buf), 0);
+                }
+                fclose(file);
+
+
+                close(sock);
+                printf("sock close\n");
+            }
             }
 
             close(sock);
@@ -63,6 +116,8 @@ int main()
     }
     
     close(listener);
+    numb_client--;
+}
 
     return 0;
 }
